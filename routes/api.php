c3bd2fa52fcdfae6096c949966079826fbff7ccb
fathurohman <fathurfurs@gmail.com>
2022-09-18 20:39:27 +0700

    add posts api

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\PostController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -25,4 +16,6 @@ Route::middleware(['auth:api'])->group(function () {
     Route::get('user', [AuthController::class, 'user']);
     Route::get('refreshtoken', [AuthController::class, 'getTokenAndRefreshToken']);
     Route::get('logout', [AuthController::class, 'logout']);
+
+    Route::resource('posts', PostController::class);
 });
